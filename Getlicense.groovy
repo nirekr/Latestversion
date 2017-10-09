1d80b599eb14b63f058fb2b541f9989ade94bf02
Ratnangi Nirek <ratnangi.nirek@dell.com>
2017-10-09 12:09:54 -0500

    Create Getlicense.groovy

---
+++ Getlicense.groovy
@@ -0,0 +1,25 @@
+#!/usr/bin/groovy
+
+import com.dell.cpsd.SCM.Utils
+
+def call() {
+    def utils = new com.dell.cpsd.SCM.Utils()
+    def publicRepos = utils.getPublicRepos()
+    println(publicRepos)
+    
+    for (repo in publicRepos) {
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${repo}"]], gitTool: 'linux-git', submoduleCfg: [], userRemoteConfigs: [[url: "https://github.com/dellemc-symphony/${repo}"]]])
+        dir("$repo"){
+            sh '''
+                 if [[ ! -f pom.xml ]]; then
+                    echo "pom file does not exist"
+                  elif grep 'version' pom.xml; then
+                    echo "The version for this file is "
+                else 
+                     echo "Version does not exist"
+                     exit 1
+                fi
+              '''
+    }        
+}
+}
